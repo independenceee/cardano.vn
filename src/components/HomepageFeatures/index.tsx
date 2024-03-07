33d974425520347dfa10867c816043e281354d5e
Le Quang Vu <leevudev@gmail.com>
2024-03-07 12:11:02 +0700

    dev

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,67 +1,65 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import React from "react";
 
-type FeatureItem = {
+type EventItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
-  description: JSX.Element;
+  image: string;
+  address: string;
 };
 
-const FeatureList: FeatureItem[] = [
+const EventsList: EventItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    image:
+      "https://www.firstevent.co.uk/wp-content/uploads/2022/07/Untitled-design-12.png",
+    address: "Berlin, Germany",
+    title: "Next Block Expo",
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    image:
+      "https://www.firstevent.co.uk/wp-content/uploads/2022/07/Untitled-design-12.png",
+    address: "Berlin, Germany",
+    title: "Next Block Expo",
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    image:
+      "https://www.firstevent.co.uk/wp-content/uploads/2022/07/Untitled-design-12.png",
+    address: "Berlin, Germany",
+    title: "Next Block Expo",
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Event({ title, image, address }: EventItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    <div className={clsx("col col--4", styles.eventItemWrapper)}>
+      <div>
+        <img className={styles.image} src={image} />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className={clsx(styles.wrapperEventInfo)}>
         <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+        <p className={clsx(styles.address)}>{address}</p>
       </div>
     </div>
   );
 }
 
-export default function HomepageFeatures(): JSX.Element {
+export default function Events(): JSX.Element {
   return (
-    <section className={styles.features}>
+    <section className={styles.events}>
+      <div className={clsx("text--center container", styles.wrapperTitle)}>
+        <Heading as="h2" className={styles.title}>
+          Event
+        </Heading>
+        <p className={styles.subTitle}>
+          Come meet us in IRL. The Aleph Zero community is active across
+          hackathons and events in the web3 space.
+        </p>
+      </div>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+        <div className={clsx(styles.eventList)}>
+          {EventsList.map((props, idx) => (
+            <Event key={idx} {...props} />
           ))}
         </div>
       </div>
