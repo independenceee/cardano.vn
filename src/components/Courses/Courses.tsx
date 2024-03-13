43c6ac8d9637426875d38b0acfc0a9838e38e2ff
Le Quang Vu <leevudev@gmail.com>
2024-03-13 16:17:44 +0700

    dev

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -12,54 +12,22 @@ import Slider from "react-slick";
 const cx = classNames.bind(styles);
 
 type Props = {
-  courses: CourseType[];
+    courses: CourseType[];
 };
 
 const Courses = function ({ courses }: Props) {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    arrows: false,
-    dots: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: { slidesToShow: 1, slidesToScroll: 1 },
-      },
-    ],
-  };
-  return (
-    <div className={cx("wrapper")}>
-      <Title
-        title="Courses"
-        subTitle="Come meet us in IRL. The Aleph Zero community is active across hackathons and events in the web3 space."
-      />
+    return (
+        <div className={cx("wrapper")}>
+            <Title
+                title="Courses"
+                subTitle="Come meet us in IRL. The Aleph Zero community is active across hackathons and events in the web3 space."
+            />
 
-      <div className="container">
-        <div className={cx("inner")}></div>
-      </div>
-    </div>
-  );
+            <div className="container">
+                <div className={cx("inner")}></div>
+            </div>
+        </div>
+    );
 };
 
 export default Courses;
